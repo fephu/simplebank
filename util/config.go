e82e545f818467288ded0f1223646a2ea2640b7f
fephu <fe.phulovely@gmail.com>
2024-11-11 17:40:29 +0000

    logs for gRPC APIS

--- util/config.go
+++ util/config.go
@@ -9,6 +9,7 @@ import (
 // Config stores all configuration of the aplication
 // The values are ready by viper from a config file or environment variables.
 type Config struct {
+	Environment          string        `mapstructure:"ENVIRONMENT"`
 	DBDriver             string        `mapstructure:"DB_DRIVER"`
 	DBSource             string        `mapstructure:"DB_SOURCE"`
 	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
